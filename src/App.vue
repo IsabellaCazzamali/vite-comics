3114c42594805a57847554827d3af54475d195c4
IsabellaCazzamali <isa.cazzamali@gmail.com>
2024-02-25 17:00:18 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<template>
+  <header>
+    <img src="/img/dc-logo.png" alt="logo DC comics">
+    <nav>
+      <ul>
+        <li>Characters</li>
+        <li>Comics</li>
+        <li>Movies</li>
+        <li>TV</li>
+        <li>Games</li>
+        <li>Collectibles</li>
+        <li>Videos</li>
+        <li>Fans</li>
+        <li>News</li>
+        <li>Shop</li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <h2>Content goes here</h2>
+  </main>
+  <footer></footer>
+</template>
+
+<style>
+
+</style>
